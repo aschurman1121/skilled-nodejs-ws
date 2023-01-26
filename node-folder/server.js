9f2205dc94d5de46c6c122d5f5f4a2f2b24198c5
Alexandra Schurman <alexandraschurman@gmail.com>
2023-01-26 15:34:48 -0800

    server running

--- node-folder/server.js
+++ node-folder/server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 5001;
 
 //creating the server 
 const server = app.listen(PORT, () => {
-    console.log(`server is listening on on PRT ${PORT}`)
+    console.log(`server is listening on on PORT ${PORT}`)
 })
 
+//gracefully shuts down reject
+process.on('unhandle rejection', (err) => {
+    console.log(`error: ${err.message}`)
+    //kills server - 1 indicates failure
+    server.close(() => process.exit(1))
+})
